1b3e6072ea19984aac047aee8c3a623a4f438748
rojojun <zoloman316@gmail.com>
2025-11-18 12:22:16 +0900

    first commit

---
+++ src/test/kotlin/com/rojojun/voidserver/VoidServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rojojun.voidserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VoidServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
