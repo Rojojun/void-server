b481d0a69bf06a063bd42abac76cfb1ed5380bfa
rojojun <zoloman316@gmail.com>
2025-12-01 23:56:20 +0900

    test: game session test code

--- src/test/kotlin/com/rojojun/voidserver/domain/service/GameStateServiceTest.kt
+++ src/test/kotlin/com/rojojun/voidserver/domain/service/GameStateServiceTest.kt
@@ -1,8 +1,12 @@
 package com.rojojun.voidserver.domain.service
 
+import com.rojojun.voidserver.domain.model.GameState
 import com.rojojun.voidserver.domain.port.out.LoadGameStatePort
 import com.rojojun.voidserver.domain.port.out.SaveGameStatePort
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 
 class GameStateServiceTest : BehaviorSpec({
@@ -11,4 +15,33 @@ class GameStateServiceTest : BehaviorSpec({
 
     val gameStateService = GameStateService(loadGameStatePort, saveGameStatePort)
 
+    Given("새로운 세션 ID가 주어졌을 때") {
+        val sessionId = 1L
+
+        coEvery { loadGameStatePort.loadSession(sessionId) } returns null
+
+        val newGameState = GameState(
+            id = sessionId,
+            act = 1,
+            logicSealBroken = false,
+            dataSealBroken = false,
+            powerSealBroken = false,
+            endingType = null,
+        )
+
+        coEvery { saveGameStatePort.saveSession(any()) } returns newGameState
+
+        When("getSession을 호출하면") {
+            val result = gameStateService.getSession(sessionId)
+
+            Then("새로운 세션이 생성되고 저장된다") {
+                result.id shouldBe sessionId
+                result.powerSealBroken shouldBe false
+            }
+
+            And("SaveGameStatePort는 호출되지 않는다") {
+                coVerify(exactly = 1) { saveGameStatePort.saveSession(any()) }
+            }
+        }
+    }
 })
